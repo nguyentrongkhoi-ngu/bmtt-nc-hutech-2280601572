fae48e2adaa0691bf844381bba70799260d6e6e4
nguyentrongkhoi <ntrkhoikk1711@gmail.com>
2025-06-18 10:22:37 +0700

    Delete LAB-01/LAB-01 directory

--- LAB-01/LAB-01/ex02/ex02_10.py
+++
@@ -1,4 +0,0 @@
-def dao_nguoc_chuoi(chuoi):
-    return chuoi[::-1]
-input_str = input("Nhập một chuỗi cần đảo ngược: ")
-print("Chuỗi đảo ngược là:", dao_nguoc_chuoi(input_str))
--- LAB-01/LAB-01/ex03/ex03_03.py
+++
@@ -1,8 +0,0 @@
-def tao_tuple_tu_list(lst):
-    return tuple(lst)
-input_list = input("Nhập một danh sách số, cách nhau bởi dấu phẩy: ")
-numbers = list(map(int, input_list.split(',')))
-
-my_tuple = tao_tuple_tu_list(numbers)
-print("List: ", numbers)
-print("Tuple từ List: ", my_tuple)
